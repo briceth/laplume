ef131a993308646790d96e5aa3e704af2b82ff81
Guillaume Corlay <guillaume.corlay-van-der-schaal@hec.edu>
2016-08-16 17:13:44 +0200

    wrote validations and dependencies, with a console crash test

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,7 +1,7 @@
 class Experience < ApplicationRecord
   belongs_to :user
 
-  validates :category, presence: true, inclusion: { in: CATEGORIES }
+  validates :category, presence: true, inclusion: { in: Mission::CATEGORIES }
   validates :title, length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
   validates :description, presence: true, length: { maximum: 500, too_long: "%{count} characters is the maximum allowed" }
   validates :organization, presence: true
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -6,5 +6,5 @@ class Mission < ApplicationRecord
   CATEGORIES = ["Biography", "Content Marketing", "Thriller", "Translation", "Romance", "Science-Fiction", "Movie Script", "Academia", "Adult"]
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   STATUS = ["pending", "on-going", "done"]
-  validates :statuts, presence: true, inclusion: { in: STATUS }, length: { minimum: 40, too_short: "%{count} characters is the minimum allowed" }
+  validates :status, presence: true, inclusion: { in: STATUS }
 end
