a933e5108ff54b3ad98f96ce4ef91022feb207af
Guillaume Corlay <guillaume.corlay-van-der-schaal@hec.edu>
2016-08-16 15:55:23 +0200

    added validations to mission model

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,3 +1,10 @@
 class Mission < ApplicationRecord
   belongs_to :user
+  has_many :offers
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  CATEGORIES = ["Biography", "Content Marketing", "Thriller", "Translation", "Romance", "Science-Fiction", "Movie Script", "Academia", "Adult"]
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+  STATUS = ["pending", "on-going", "done"]
+  validates :statuts, presence: true, inclusion: { in: STATUS }, length: { minimum: 40, too_short: "%{count} characters is the minimum allowed" }
 end
