0002fe7d60196af924b5dda600346595e01c0967
Guillaume Corlay <guillaume.corlay-van-der-schaal@hec.edu>
2016-08-17 00:09:24 +0200

    added to routes for users (edit and update) + worked on the user#show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @experiences = @user.experiences
+    @work_experiences = @experiences.select { |experience| experience.category == "Experience" }
+    @educations = @experiences.select { |experience| experience.category == "Education" }
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
