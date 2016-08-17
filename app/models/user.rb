017a444a2d5558d5dc24286e4428a5a26bd7d7bf
Vincent Bres <bres.vincent@gmail.com>
2016-08-17 14:39:04 +0200

    finish cloudinary & attachinary setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attachment :photo
+
   has_many :experiences
   has_many :missions
   has_many :offers
