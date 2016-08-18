456f9f314cd369daf79884af8813da260c4ff40b
Guillaume Corlay <guillaume.corlay-van-der-schaal@hec.edu>
2016-08-18 16:26:32 +0200

    fixed bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:home, :search, :user]
+  before_action :authenticate_user!
 
   protect_from_forgery with: :exception
 end
